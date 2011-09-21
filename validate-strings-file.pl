c7bc9c0e04876bb90fbed5ce316c07fb7c3a8101
Chase Johnson <chase.johnson@semaphoremobile.com>
2011-09-21 12:49:53 -0500

    Initial commit

---
+++ validate-strings-file.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+open(STRINGS_FILE, $ARGV[0]);
+@lines = <STRINGS_FILE>;
+$linenum = 1;
+$errorcount = 0;
+foreach $line (@lines) {
+	if (!($line =~ m/\w*?\s*?=\s*?"[^"]*?"\;/ || $line =~ m/^\/\/.*$/ || $line =~ m/^\s*$/)) {
+		print "$linenum: $line";
+		$errorcount++;
+	}
+	$linenum++;
+}
+print "$errorcount errors found in strings file.\n"
+
